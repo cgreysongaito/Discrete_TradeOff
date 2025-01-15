e79186e35a4bd850801f1c6465440d66cfd2cdea
Christopher J. Greyson-Gaito <christopher@greyson-gaito.com>
2025-01-15 15:12:56 -0500

    coded iteration of beverton-holt time delay model

--- src/TradeOffs_BevertonHolt.jl
+++ src/TradeOffs_BevertonHolt.jl
@@ -1,16 +1,40 @@
-include(packages.jl)
+include("packages.jl")
 
-function BevertonHolt_model(para)
-        N[t] = N[t-1]  / (1 + α + β* N[t-1] ) + (a-b*exp(-K(τ+1)))*(p^(τ+1))*N[t-τ]
-    return N
+@with_kw mutable struct BevHoltPar
+    α::Float64 = 0.1
+    β::Float64 = 0.3
+    a::Float64 = 10
+    b::Float64 = 200
+    K::Float64 = 1
+    p::Float64 = 0.4
+    τ::Int = 5
+end
+
+function calc_m(para)
+    @unpack a,b,K,p,τ = para
+    m = (a-b*exp(-K*(τ+1)))*(p^(τ+1))
+    return m
+end
+
+calc_m(BevHoltPar())
+
+function BevertonHolt_model(Ndata, t, para)
+    @unpack α,β,a,b,K,p,τ = para
+        return (Ndata[t]  / (1 + α + β* Ndata[t] )) + (a-b*exp(-K*(τ+1)))*(p^(τ+1))*Ndata[t-τ]
 end
 
 function BevertonHolt_recursion(N0,time,para)
-    N = zeros(time)
-    N[1] = N0
-    for t in 2:time
-        N[t] = BevertonHolt_model(N[t-1],para)
+    @unpack τ = para
+    N = fill(N0, τ+1)
+    for t in τ+1:τ+time
+        newN = BevertonHolt_model(N,t,para)
+        append!(N,newN)
     end
-    return N
+    return N[τ+1:end]
 end
-#TODO need to figure out history
+
+let 
+    time = 500
+    timeseries = BevertonHolt_recursion(0.1,time,BevHoltPar(τ=2,p=0.55))
+    plot(0:1:time,timeseries)
+end
